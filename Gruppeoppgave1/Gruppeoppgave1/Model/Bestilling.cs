e3f14b2457e4fcf14af901a614eb835af46c1208
benjamhw <benjamin.99@live.no>
2020-09-17 15:47:03 +0200

    Lagt til interfaces

--- Gruppeoppgave1/Gruppeoppgave1/Model/Bestilling.cs
+++ Gruppeoppgave1/Gruppeoppgave1/Model/Bestilling.cs
@@ -1,13 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gruppeoppgave1.Model
 {
     public class Bestilling
     {
-        public int id { get; set; }
-
+        [Key]
+        public int Id { get; set; }
+        public double pris { get; set; }
+        public string Fra { get; set; }
+        public string Til { get; set; }
+        public string Dato { get; set; }
+        public string Tid { get; set; }
     }
 }

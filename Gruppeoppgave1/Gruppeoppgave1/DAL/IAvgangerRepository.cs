1d7af1a6aafe1ec52636ec9e293a525a8d029810
benjamhw <benjamin.99@live.no>
2020-09-17 15:47:03 +0200

    Lagt til interfaces

---
+++ Gruppeoppgave1/Gruppeoppgave1/DAL/IAvgangerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gruppeoppgave1.Model;
+
+namespace Gruppeoppgave1.DAL
+{
+    public interface IAvgangerRepository
+    {
+        Task<List<Avganger>> HentAlle();
+        Task<Avganger> HentEn(int id);
+    }
+}

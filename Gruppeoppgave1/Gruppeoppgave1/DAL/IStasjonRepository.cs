09422a6974ffdd694925c8282a71ad5ca709b22c
benjamhw <benjamin.99@live.no>
2020-09-17 15:47:03 +0200

    Lagt til interfaces

---
+++ Gruppeoppgave1/Gruppeoppgave1/DAL/IStasjonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gruppeoppgave1.Model;
+
+namespace Gruppeoppgave1.DAL
+{
+    public interface IStasjonRepository
+    {
+        Task<List<Stasjon>> HentAlle();
+        Task<Stasjon> HentEn(int id);
+    }
+}

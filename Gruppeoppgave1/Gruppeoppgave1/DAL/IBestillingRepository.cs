ae1ac0e73d2e6114564289ed504feaf6c48caead
benjamhw <benjamin.99@live.no>
2020-09-17 14:05:30 +0200

    Endring i struktur

---
+++ Gruppeoppgave1/Gruppeoppgave1/DAL/IBestillingRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gruppeoppgave1.Model;
+
+namespace Gruppeoppgave1.DAL
+{
+    public interface IBestillingRepository
+    {
+        Task<bool> Lagre(Bestilling innBestilling);
+        Task<List<Bestilling>> HentAlle();
+        Task<bool> Slett(int id);
+        Task<Bestilling> HentEn(int id);
+        Task<bool> Endre(Bestilling endreBestilling);
+    }
+}

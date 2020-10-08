bea596555e673ebeae588fb58acd39bfef6c41a9
MTK <tayyabkhalid306@gmail.com>
2020-10-08 14:12:27 +0200

    Lagt til Kunde og Admin models

---
+++ Gruppeoppgave1/Gruppeoppgave1/Model/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gruppeoppgave1.Model
+{
+    public class Admin
+    {
+        public string username { get; set; }
+        public string passord { get; set; }
+
+    }
+}

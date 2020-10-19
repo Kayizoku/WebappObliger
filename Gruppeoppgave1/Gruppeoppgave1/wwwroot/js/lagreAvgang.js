f531eaf1394dff206fa78a3264024d9572bdf40a
eskilnysether <s330457@oslomet.no>
2020-10-19 19:05:09 +0200

    Admin

---
+++ Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/lagreAvgang.js
@@ -0,0 +1,16 @@
+﻿function lagreAvgang() {
+    const avgang = {
+        fra: $("#FraFelt").val(),
+        til: $("#TilFelt").val(),
+        tid: $("#TidFelt").val()
+    }
+    const url = "avganger/leggTilAvgang";
+    $.post(url, avgang, function (OK) {
+        if (OK) {
+            window.location.href = 'admin.html';
+        }
+        else {
+            $("#feil").html("Feil i db - prøv igjen senere");
+        }
+    });
+};

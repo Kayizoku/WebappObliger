ecabdcc86144ddf33152cdf9e8226789be26d9ed
Amir Arsalan Nouyan <amirarsalannouyan@gmail.com>
2020-10-20 14:14:40 +0200

    Merget Melissa sitt med det som var der

--- Gruppeoppgave1/Gruppeoppgave1/DAL/Repositories/BestillingRepository.cs
+++ Gruppeoppgave1/Gruppeoppgave1/DAL/Repositories/BestillingRepository.cs
@@ -12,16 +12,19 @@ using Microsoft.AspNetCore.Mvc;
 using Gruppeoppgave1.DAL.IRepositories;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Security.Cryptography;
+using Microsoft.Extensions.Logging;
 
-namespace Gruppeoppgave1.DAL.IRepositories
+namespace Gruppeoppgave1.DAL.Repositories
 {
     [ExcludeFromCodeCoverage]
     public class BestillingRepository : IBestillingRepository
     {
         private readonly BestillingContext _db;
+        private ILogger<BestillingRepository> _log;
 
-        public BestillingRepository(BestillingContext db)
+        public BestillingRepository(BestillingContext db, ILogger<BestillingRepository> log)
         {
+            _log = log;
             _db = db;
         }
 
@@ -40,8 +43,9 @@ namespace Gruppeoppgave1.DAL.IRepositories
                 await _db.SaveChangesAsync();
                 return true;
             }
-            catch
+            catch (Exception e)
             {
+                _log.LogError(e.Message);
                 return false;
             }
         }
@@ -59,26 +63,27 @@ namespace Gruppeoppgave1.DAL.IRepositories
                     Dato = b.Dato,
                     Tid = b.Tid
                 }).ToListAsync();
-                
+
                 return alleBestillinger;
             }
-            catch
+            catch (Exception e)
             {
+                _log.LogError(e.Message);
                 return null;
-            }  
+            }
         }
 
         public async Task<bool> Slett(int id)
         {
-
             try
             {
                 Bestillinger enDBBestilling = await _db.Bestillinger.FindAsync(id);
                 _db.Bestillinger.Remove(enDBBestilling);
                 await _db.SaveChangesAsync();
             }
-            catch
+            catch (Exception e)
             {
+                _log.LogError(e.Message);
                 return false;
             }
             return true;
@@ -100,14 +105,16 @@ namespace Gruppeoppgave1.DAL.IRepositories
                 };
                 return hentetBestilling;
             }
-            catch
+            catch (Exception e)
             {
+                _log.LogError(e.Message);
                 return null;
             }
         }
 
         public async Task<bool> Endre(Bestilling endreBestilling)
         {
+
             try
             {
                 var endreObjekt = await _db.Bestillinger.FindAsync(endreBestilling.Id);
@@ -118,13 +125,16 @@ namespace Gruppeoppgave1.DAL.IRepositories
                 endreObjekt.Tid = endreBestilling.Tid;
                 await _db.SaveChangesAsync();
             }
-            catch
+            catch (Exception e)
             {
+                _log.LogError(e.Message);
                 return false;
             }
             return true;
-            
+
         }
+
+    
 
         public static byte[] Hash(string passord, byte[] salt)
         {
@@ -160,7 +170,7 @@ namespace Gruppeoppgave1.DAL.IRepositories
             }
             catch (Exception e)
             {
-               // _log.LogInformation(e.Message);
+                _log.LogInformation(e.Message);
                 return false;
             }
         }

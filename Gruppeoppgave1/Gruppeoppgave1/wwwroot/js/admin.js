d236a08d0c8cd3874b505390423f5e410917d097
eskilnysether <s330457@oslomet.no>
2020-10-22 13:17:13 +0200

    Ruter

--- Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/admin.js
+++ Gruppeoppgave1/Gruppeoppgave1/wwwroot/js/admin.js
@@ -98,17 +98,17 @@ function hentAlleRuter() {
 function formaterRuter(ruter) {
     let ut = "<table class='table table-striped'>" +
         "<tr>" +
-        "<th>Pris</th><th>Fra</th><th>Til</th><th>Dato</th><th>Tid</th><th></th><th></th>" +
+        "<th>Navn</th><th>Stasjoner på rute</th><th></th><th></th>" +
         "</tr>";
     for (let rute of ruter) {
         ut += "<tr>" +
             "<td>" + rute.navn + "</td>" +
             "<td>" + bestilling.stasjonerPaaRute + "</td>" +
-            "<td><a class='btn btn-primary' href='endreBestilling.html?id=" + bestilling.id + "'>Endre</a></td >" +
-            "<td> <button class='btn btn-danger' onclick='slettEnBestilling()" + bestilling.id + ")'>Slett</button></td>" +
+            "<td><a class='btn btn-primary' href='endreRute.html?id=" + rute.id + "'>Endre</a></td >" +
+            "<td> <button class='btn btn-danger' onclick='slettRute()" + rute.id + ")'>Slett</button></td>" +
             "</tr>";
     }
     ut += "</table>";
-    $("#bestillingene").html(ut);
+    $("#rutene").html(ut);
 }
         
